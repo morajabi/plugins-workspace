279e3d145de6a52ac8f6e362f52fe33e4260fb83
Mohamad <mo.rajbi@gmail.com>
2023-03-15 09:49:18 +0330

    Revert "fix(log): use Swift OSLog instead of oslog Rust binding (#262)"
    This reverts commit 961602bd1b15e9c0d83d4c32c221a075fbabd83b.

--- plugins/log/build.rs
+++ plugins/log/build.rs
@@ -1,15 +1,12 @@
-// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
-// SPDX-License-Identifier: Apache-2.0
-// SPDX-License-Identifier: MIT
-
-use std::process::exit;
+fn alias(alias: &str, has_feature: bool) {
+    if has_feature {
+        println!("cargo:rustc-cfg={alias}");
+    }
+}
 
 fn main() {
-    if let Err(error) = tauri_build::mobile::PluginBuilder::new()
-        .ios_path("ios")
-        .run()
-    {
-        println!("{error:#}");
-        exit(1);
-    }
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let mobile = target_os == "ios" || target_os == "android";
+    alias("desktop", !mobile);
+    alias("mobile", mobile);
 }
